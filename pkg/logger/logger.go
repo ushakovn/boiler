20b9ad9afa2713764850ad7a699f534664b66609
UshakovN <ushakovnika@mpei.ru>
2025-02-07 16:31:14 +0300

    fix: fixed json formatter print

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -11,14 +11,10 @@ func SetDefaultLogOptions() {
   if appEnv == env.LocalEnv {
     // Set text formatter for local run
     log.SetFormatter(&log.TextFormatter{
-      FullTimestamp:   true,
       TimestampFormat: "2006-01-02 15:04:05",
     })
     return
   }
   // Set JSON formatter otherwise
-  log.SetFormatter(&log.JSONFormatter{
-    PrettyPrint:     true,
-    TimestampFormat: "2006-01-02 15:04:05",
-  })
+  log.SetFormatter(new(log.JSONFormatter))
 }
